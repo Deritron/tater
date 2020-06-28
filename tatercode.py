1e45957f5ee32b62c093b3fb4ab1088efb7326fc
TransporterVIT <56848863+TransporterVIT@users.noreply.github.com>
2020-06-28 21:17:28 +0200

    Create tatercode.py

---
+++ tatercode.py
@@ -0,0 +1,41 @@
+import time
+import datetime
+import RPi.GPIO as GPIO
+
+#initialisatie pinnetje
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD) #we gebruiken de fysieke pin nummer(pinout)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) #bijopstarten staat pinnetje altijd af
+
+
+#watertijd is de duurtijd in seconde dat hij water zal geven
+watertijd = 4
+
+
+
+#beginnen met water geven
+print ("Ik begin nu met water te geven voor:", watertijd, " seconden.")
+
+logbestand=open("/webapp/taterlog.txt","a+")
+nu=datetime.datetime.now()
+huidigetijd = str(nu)
+print(huidigetijd)
+logbestand.write(huidigetijd)
+logbestand.write("\r\n Ik begin nu water te geven \r\n")
+
+GPIO.output(8, GPIO.HIGH)
+
+#start timer
+time.sleep(watertijd)
+
+GPIO.output(8, GPIO.LOW)
+print ("Ik ben nu klaar met water geven.")
+
+#logbestand=open("/webapp/taterlog.txt","a+")
+nu=datetime.datetime.now()
+huidigetijd = str(nu)
+print(huidigetijd)
+logbestand.write(huidigetijd)
+logbestand.write("\r\n Ik ben klaar met water geven \r\n")
+logbestand.write("***************************************** \r\n")
+
