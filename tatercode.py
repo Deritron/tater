4163f088100ae1225b1f9c59f32807ef3536088d
Tongfa17 <67696307+Tongfa17@users.noreply.github.com>
2020-07-01 16:14:54 +0200

    Update tatercode.py

--- tatercode.py
+++ tatercode.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 GPIO.cleanup()
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD) #we gebruiken de fysieke pin nummer in de plaats van de GPIO nummer. (zie commando "pinout" op de Raspberry Pi
-GPIO.setup(8, GPIO.OUT) #bijopstarten staat pinnetje altijd af
+GPIO.setup(3, GPIO.OUT) #bijopstarten staat pinnetje altijd af
 
 
 #watertijd is de duurtijd in seconde dat hij water zal geven
@@ -25,12 +25,12 @@ print(huidigetijd)
 logbestand.write(huidigetijd)
 logbestand.write("\r\nIk begin nu water te geven.\r\n")
 
-GPIO.output(8, GPIO.LOW)
+GPIO.output(3, GPIO.LOW)
 
 #start timer
 time.sleep(watertijd)
 
-GPIO.output(8, GPIO.HIGH)
+GPIO.output(3, GPIO.HIGH)
 print ("Ik ben nu klaar met water geven.")
 #GPIO.cleanup()
 
